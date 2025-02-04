f45d7d9f4f78ee3dd55240e306134921d41b86b2
AyakaKoyama <ayamail2000@gmail.com>
2025-02-04 20:53:46 +0900

    jest-setup

--- src/backend/spec/models/post_spec.rb
+++ src/backend/spec/models/post_spec.rb
@@ -1,40 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  
-  it "タイトル、コンテンツ、ユーザー名があること" do
-  post = Post.new(
-    title: "title",
-    content: "content",
-    username: "username"
-  )
-  expect(post).to be_valid
+  context '有効な場合' do
+    it 'タイトル、コンテンツ、ユーザー名があること' do
+      post = Post.new(title: 'title', content: 'content', username: 'username')
+      expect(post).to be_valid
+    end
   end
 
-  it "タイトルがなければ無効であること" do
-  post = Post.new(title: nil)
-  post.valid?
-  expect(post.errors[:title]).to include("can't be blank")
-  end
+  context '無効な場合' do
+    it 'タイトルがなければ無効であること' do
+      post = Post.new(title: nil)
+      post.valid?
+      expect(post.errors[:title]).to include('can\'t be blank')
+    end
 
-  it "ユーザー名がなければ無効であること" do
-  post = Post.new(username: nil)
-  post.valid?
-  expect(post.errors[:username]).to include("can't be blank")
-  end
+    it 'ユーザー名がなければ無効であること' do
+      post = Post.new(username: nil)
+      post.valid?
+      expect(post.errors[:username]).to include('can\'t be blank')
+    end
 
-  it "重複したユーザー名があれば無効であること" do
-  Post.create(
-    title: "title",
-    content: "content",
-    username: "username"
-  )
-  post = Post.new(
-    title: "title",
-    content: "content",
-    username: "username"
-  )
-  post.valid?
-  expect(post.errors[:username]).to include("has already been taken")
+    it '重複したユーザー名があれば無効であること' do
+      Post.create(title: 'title', content: 'content', username: 'username')
+      post = Post.new(title: 'title', content: 'content', username: 'username')
+      post.valid?
+      expect(post.errors[:username]).to include('has already been taken')
+    end
   end
 end
