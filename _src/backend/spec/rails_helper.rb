16f14da78b8903a174cb1dc8f175d21e11f5296e
AyakaKoyama <ayamail2000@gmail.com>
2025-02-10 07:45:03 +0900

    create rspec

--- _src/backend/spec/rails_helper.rb
+++ _src/backend/spec/rails_helper.rb
@@ -1,24 +1,20 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-
-# rspec-rails を使用するための設定
-require 'spec_helper'
 # 本番や開発データベースを使用しないようにするための設定
 ENV['RAILS_ENV'] ||= 'test'
 # Rails の環境をテストモードに設定
 require File.expand_path('../config/environment', __dir__)
 
-# RSpec の実行環境が production になっていないかチェック
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'rspec/rails'
+# rspec-rails を使用するための設定
+require 'spec_helper'
+require_relative '../config/environment'
+
+
+# RSpec の実行環境が production になっていないかチェック
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 
 # （FactoryBot）spec/support ディレクトリ内のすべてのファイルを読み込む
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -33,29 +29,10 @@ RSpec.configure do |config|
   # テストが DB を変更した場合にリセットする設定（RSpec の transactions を使用）
   config.use_transactional_fixtures = true
 
-  # You can uncomment this line to turn off ActiveRecord support entirely.
-  # config.use_active_record = false
-
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, type: :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://rspec.info/features/7-0/rspec-rails
+  # テストが失敗した場合にスクリーンショットを撮る設定
   config.infer_spec_type_from_file_location!
-
-  # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
-  
+
   # 不要な `should` の警告を抑制
   config.expect_with :rspec do |expectations|
     expectations.syntax = :expect
--- _src/backend/spec/requests/post_spec.rb
+++ _src/backend/spec/requests/post_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
+
 RSpec.describe 'Posts', type: :request do
+  let(:valid_params) do
+    { post: { title: 'Title', content: 'Content', username: 'Username' } }
+  end
+
+  let(:valid_post) do
+    FactoryBot.create(:post, title: 'Title', content: 'Content', username: 'Username')
+  end
+
   describe 'GET /index' do
     it 'returns http sucess status' do
       get '/api/v1/posts'
@@ -10,16 +19,31 @@ RSpec.describe 'Posts', type: :request do
 
   describe 'POST /create' do
     it 'returns http success status' do
-      post '/api/v1/posts', params: { post: { title: 'Title', content: 'Content', username: 'Username' } }
+      post '/api/v1/posts', params: valid_params
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
     it 'returns http success status' do
-      post = FactoryBot.create(:post, title: 'Title', content: 'Content', username: 'Username')
-      expect(post).to be_valid
-      get "/api/v1/posts/#{post.id}" # 文字列補間を使用しているのでダブルクォートのまま
+      post = valid_post
+      get "/api/v1/posts/#{post.id}"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'PUT /update' do
+    it 'returns http success status' do
+      post = valid_post
+      put "/api/v1/posts/#{post.id}", params: valid_params
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    it 'returns http success status' do
+      post = valid_post
+      delete "/api/v1/posts/#{post.id}"
       expect(response).to have_http_status(:success)
     end
   end
